8241328b05353173ee336a0109f1e96943cff754
sherman <sherman@bambu.co>
2021-07-31 10:37:27 +0800

    added basic loop for going through datasets

--- section1/section1.py
+++ section1/section1.py
@@ -4,16 +4,22 @@ import os
 import types
 
 
+
+
 def job():
     print("hello world")
     workingDirectory = os.path.dirname(os.path.realpath(__file__))
-    dataset1_out = open(f"{workingDirectory}/dataset1_processed.csv")
+
+    for i in range(1, 3):
+        outFile = open(f"{workingDirectory}/dataset{str(i)}_processed.csv")
+        with open(f"{workingDirectory}/dataset{str(i)}.csv", "w+") as f:
+            pass
+        outFile.close()
 
 
-
-if __name__ == "__main__":
-    sched = BlockingScheduler()
-    trigger = CronTrigger(hour=1)
-    sched.add_job(job, trigger=trigger)
-    sched.start()
+# if __name__ == "__main__":
+#     sched = BlockingScheduler()
+#     trigger = CronTrigger(hour=1)
+#     sched.add_job(job, trigger=trigger)
+#     sched.start()
 

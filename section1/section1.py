a5b87e4a2f97da3ecb49cb2f75a952ad17e9fb6c
sherman <sherman@bambu.co>
2021-07-31 10:54:19 +0800

    added clean line function and integrated with job

--- section1/section1.py
+++ section1/section1.py
@@ -1,25 +1,55 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 import os
-import types
+from typing import Tuple
 
 
+def cleanLine(string: str) -> Tuple[bool, str]:
+    if string.split(",")[0] == "name":
+        return True, "first_name,last_name,price,above_100"
+    if string.split(",")[0] == "":
+        return False, ""
+
+    name = string.split(",")[0]
+    price = string.split(",")[1]
+
+    nameArr = name.split(" ")
+    lastName = nameArr[-1]
+    firstName = nameArr[0]
+    if len(nameArr) > 2:
+        firstName = nameArr[1]
+    elif len(nameArr) == 1:
+        firstName = nameArr[0]
+        lastName = ""
+
+    above100 = ""
+    try:
+        cleanPrice = float(price)
+        if cleanPrice > 100:
+            above100 = "true"
+    except ValueError:
+        print("invalid price")
+        return False, ""
+
+    return True, f"{firstName},{lastName},{str(cleanPrice)},{above100}\n"
 
 
 def job():
     print("hello world")
     workingDirectory = os.path.dirname(os.path.realpath(__file__))
 
-    for i in range(1, 3):
-        outFile = open(f"{workingDirectory}/dataset{str(i)}_processed.csv")
-        with open(f"{workingDirectory}/dataset{str(i)}.csv", "w+") as f:
-            pass
+    for i in range(1, 2):
+        outFile = open(f"{workingDirectory}/dataset{str(i)}_processed.csv", "w+")
+        with open(f"{workingDirectory}/dataset{str(i)}.csv") as f:
+            for line in f:
+                resBool, resString = cleanLine(line.strip())
+                if resBool:
+                    outFile.write(resString)
         outFile.close()
 
-
-# if __name__ == "__main__":
-#     sched = BlockingScheduler()
-#     trigger = CronTrigger(hour=1)
-#     sched.add_job(job, trigger=trigger)
-#     sched.start()
+if __name__ == "__main__":
+    sched = BlockingScheduler()
+    trigger = CronTrigger(hour=1)
+    sched.add_job(job, trigger=trigger)
+    sched.start()
 

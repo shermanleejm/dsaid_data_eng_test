201b66fc22e4b43b323558df7406e5c5f8be68c0
sherman <leesherman@live.com.sg>
2021-07-31 19:16:35 +0800

    initial commit for visualisation

---
+++ section4/src/components/DataConnector.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import axios from 'axios';
+
+class DataConnector {
+  private _rootPath: string;
+  constructor() {this._rootPath =  }
+  
+
+}
+
+export default DataConnector;
